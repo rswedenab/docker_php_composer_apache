6afffbba2477f3c7b7fa161b493e57293b467efc
rswedenab <31689764+rswedenab@users.noreply.github.com>
2018-03-14 09:51:03 +0100

    Update and rename start.sh to run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+if [ -e /etc/apache2/custom_start.sh ]
+  /etc/apache2/custom_start.sh
+else
+  /usr/sbin/apache2ctl -D FOREGROUND
+fi
--- start.sh
+++
@@ -1,5 +0,0 @@
-if [ -e custom_start.sh ]
-  ./custom_start.sh
-fi
-
-CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
