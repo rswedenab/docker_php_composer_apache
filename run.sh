1773b171b7b31ff7954c4de9a174ec19259999b2
rswedenab <31689764+rswedenab@users.noreply.github.com>
2018-03-20 11:41:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -e /etc/apache2/custom_start.sh ]; then
-  /etc/apache2/custom_start.sh
+if [ -e /custom_config/custom_start.sh ]; then
+  /custom_config/custom_start.sh
 else
   /usr/sbin/apache2ctl -D FOREGROUND
 fi
